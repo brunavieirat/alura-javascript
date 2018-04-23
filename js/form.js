7b693032921cbbf3573e1594ad7de394c295a7ca
brunavieirat <bruna.vieira.t@hotmail.com>
2018-04-22 21:55:11 -0300

    separando js (criado arquivo form.js

---
+++ js/form.js
@@ -0,0 +1,39 @@
+
+var buttonAdd = document.querySelector("#adicionar-paciente");
+
+buttonAdd.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    console.log(nome)
+
+    var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    console.log(pacienteTr);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
+});
+
