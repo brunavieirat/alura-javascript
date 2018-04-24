2aa22e63fd3f375fb05b2e9170c4f2864fbb9b86
brunavieirat <bruna.vieira.t@hotmail.com>
2018-04-24 11:11:31 -0300

    Remove TODOS TABLE  dblClick

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,9 +1,28 @@
-//alert('ok');
+// //alert('ok');
 
-var pacientes = document.querySelectorAll(".paciente");
-console.log(pacientes);
+//REMOVE PACIENTES EXISTENTES
 
-pacientes.forEach(function(paciente){
-    paciente.addEventListener('dblclick', function(){
-         this.remove()})
+// var pacientes = document.querySelectorAll(".paciente");
+// console.log(pacientes);
+
+// pacientes.forEach(function(paciente){
+//     paciente.addEventListener('dblclick', function(){
+//          this.remove()})
+// });
+
+//REMOVE TODOS PACIENTES INCLUSIVE OS QUE FOREM ADD NA TABELA
+
+// var tabela = document.querySelector("table");
+
+// tabela.addEventListener("dblclick", function(event) {
+//     var alvoEvento = event.target;
+//     var paiDoAlvo = alvoEvento.parentNode; // TR = paciente = remover 
+//     paiDoAlvo.remove();
+// });
+
+//MELHORANDO O CODIGO
+var tabela = document.querySelector("table");
+
+tabela.addEventListener("dblclick",function(event){
+    event.target.parentNode.remove();
 });
